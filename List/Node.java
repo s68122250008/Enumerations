58d56c584876f080476a90d94bc7a777952368c5
s67122250096-pixel <s68122250008@ssru.ac.th>
2026-01-22 10:19:38 +0700

    Create TestNode

--- List/Node.java
+++ List/Node.java
@@ -3,20 +3,26 @@ package List;
 class Node {
 
     // --- ส่วนประกาศตัวแปร (Fields) ---
-    int data;    // ตัวแปรเก็บข้อมูล (ในที่นี้เก็บตัวเลขจำนวนเต็ม)
-    Node next;   // ตัวแปรเก็บ "ที่อยู่" ของ Node ถัดไป (เอาไว้ชี้หากัน)
+    private int data;    // ตัวแปรเก็บข้อมูล (ในที่นี้เก็บตัวเลขจำนวนเต็ม)
+    private Node next;   // ตัวแปรเก็บ "ที่อยู่" ของ Node ถัดไป (เอาไว้ชี้หากัน)
+    private static int count;
 
     // --- Constructors (ตัวสร้างวัตถุ) ---
     // แบบที่ 1: สร้าง Node โดยใส่แค่ข้อมูล (ยังไม่เชื่อมต่อกับใคร)
     public Node(int dataValue) {
         data = dataValue;  // เอาค่าที่รับมาใส่ในตัวแปร data
         next = null;       // ให้ next เป็น null เพราะยังไม่มีตัวถัดไป
+        ++count;
     }
 
     // แบบที่ 2: สร้าง Node พร้อมระบุตัวถัดไปทันที
     public Node(int dataValue, Node nextValue) {
         data = dataValue;  // ใส่ข้อมูล
         next = nextValue;  // ชี้ไปหา Node ถัดไปทันที
+    }
+
+    public static int getCount() {
+        return count;
     }
 
     // --- Getter Methods (ขอดูค่า) ---
@@ -40,37 +46,5 @@ class Node {
     public void setNext(Node nextValue) {
         next = nextValue;
     }
-
-    // --- ส่วนทดสอบการทำงาน (Main Class) ---
-    public class TestNode {
-
-        public static void main(String[] args) {
-
-            // 1. สร้าง Node ขึ้นมา 2 ตัว (ยังแยกกันอยู่ อิสระจากกัน)
-            Node n1 = new Node(1); // n1 เก็บเลข 1
-            Node n2 = new Node(2); // n2 เก็บเลข 2
-
-            System.out.println("Before "); // Before
-            System.out.println("n1 = " + n1); // ปริ้นที่อยู่ของ n1
-
-            // ตอนนี้ n1 ยังไม่ชี้ใคร ค่าที่ได้จะเป็น null
-            System.out.println("next of n1 " + n1.getNext());
-
-            System.out.println("n2 = " + n2);
-            System.out.println("next of n2 " + n2.getNext());
-
-            System.out.println("After "); // ปริ้นสถานะหลังเชื่อม
-
-            // 2. ทำการเชื่อมต่อ! (Link)
-            // สั่งให้ n1 ชี้ไปหา n2 (เอาตะขอ n1 ไปเกี่ยว n2)
-            n1.setNext(n2);
-
-            // ตอนนี้ n1 จะต้องรู้จักรหัสที่อยู่ของ n2 แล้ว (ไม่เป็น null แล้ว)
-            System.out.println("next of n1 = " + n1.getNext());
-
-            // n2 ยังไม่ได้ชี้ใคร ก็จะเป็น null เหมือนเดิม
-            System.out.println("next of n2 = " + n2.getNext());
-        }
-    }
-
+    
 }
